5b8846cb29cd024db637f0a5436a3086863a185e
BitWonder <drake@morgancomputers.net>
2023-08-03 09:20:16 -0500

    adds commits a clear screen and glider gun!

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use std::io::{self, Write};
+use std::{io::{self, Write}, thread, time::Duration};
 
 #[derive(Debug)]
+
+#[derive(Clone)]
 struct Game {
     cells : Vec<Vec<State>>
 }
@@ -10,7 +12,10 @@ impl Game {
         Game { cells : vec![vec![State::Dead; square_area]; square_area] }
     }
 
-    fn step(self) -> Game {
+    /*
+        takes the game and steps it in time buy one following conway's rules
+    */
+    fn step(&mut self) {
         let mut next = Game::new( self.cells.len() );
         for x in 0..self.cells.len() {
             for y in 0..self.cells[x].len() {
@@ -39,9 +44,12 @@ impl Game {
                 }
             }
         }
-        return next;
+        *self = next;
     }
 
+    /*
+        finds the number of neighbors a cell has that's alive
+    */
     fn count(&self, position_x : isize, position_y : isize) -> u64 {
         let mut count : u64 = 0;
         for x in (position_x - 1) as isize ..= (position_x + 1) as isize {
@@ -68,7 +76,11 @@ impl Game {
         return count;
     }
 
+    /*
+        Says on the can
+     */
     pub fn print_game(&self) {
+        clear_screen();
         for x in 0..self.cells.len() {
             for y in 0..self.cells[x].len() {
                 match self.cells[y][x] {
@@ -79,7 +91,6 @@ impl Game {
             print!("\n");
             io::stdout().flush().unwrap();
         }
-        println!("\n \n \n");
     }
 }
 
@@ -89,13 +100,70 @@ enum State {
     Dead
 }
 
+fn clear_screen() {
+    // if windows or unix then clear screen
+    if cfg!(target_os = "windows") {
+        // clears the windows screen
+        std::process::Command::new("cmd")
+            .args(["/c", "cls"])
+            .spawn()
+            .expect("cls command failed to start")
+            .wait()
+            .expect("failed to wait");
+    } else {
+        // clears the unix screen
+        std::process::Command::new("clear")
+            .spawn()
+            .expect("clear command failed to start")
+            .wait()
+            .expect("failed to wait");
+    }
+}
+
 fn main() {
-    let mut game = Game::new( 5 );
-    game.cells[0][1] = State::Alive;
-    game.cells[1][1] = State::Alive;
-    game.cells[2][1] = State::Alive;
+    let mut game = Game::new( 40 );
+    game.cells[5][1] = State::Alive;
+    game.cells[5][2] = State::Alive;
+    game.cells[6][1] = State::Alive;
+    game.cells[6][2] = State::Alive;
+
+    game.cells[3][13] = State::Alive;
+    game.cells[3][14] = State::Alive;
+    game.cells[4][12] = State::Alive;
+    game.cells[4][16] = State::Alive;
+    game.cells[5][11] = State::Alive;
+    game.cells[5][17] = State::Alive;
+    game.cells[6][11] = State::Alive;
+    game.cells[6][15] = State::Alive;
+    game.cells[6][17] = State::Alive;
+    game.cells[6][18] = State::Alive;
+    game.cells[7][11] = State::Alive;
+    game.cells[7][17] = State::Alive;
+    game.cells[8][12] = State::Alive;
+    game.cells[8][16] = State::Alive;
+    game.cells[9][13] = State::Alive;
+    game.cells[9][14] = State::Alive;
+
+    game.cells[1][25] = State::Alive;
+    game.cells[2][23] = State::Alive;
+    game.cells[2][25] = State::Alive;
+    game.cells[3][21] = State::Alive;
+    game.cells[3][22] = State::Alive;
+    game.cells[4][21] = State::Alive;
+    game.cells[4][22] = State::Alive;
+    game.cells[5][21] = State::Alive;
+    game.cells[5][22] = State::Alive;
+    game.cells[6][23] = State::Alive;
+    game.cells[6][25] = State::Alive;
+    game.cells[7][25] = State::Alive;
+
+    game.cells[3][35] = State::Alive;
+    game.cells[3][36] = State::Alive;
+    game.cells[4][35] = State::Alive;
+    game.cells[4][36] = State::Alive;
     'game_loop : loop {
         game.print_game();
-        game = game.step();
+        game.step();
+        thread::sleep(Duration::from_millis(5));
     }
 }
